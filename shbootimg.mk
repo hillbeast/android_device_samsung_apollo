e03c6fe7b63fedb11c94681bd8cb40f0ed6baffc
Mark Kennard <komcomputers@gmail.com>
2012-05-07 23:00:10 +1200

    Generate ramdisk.cpio for kernel

--- shbootimg.mk
+++ shbootimg.mk
@@ -3,9 +3,11 @@ LOCAL_PATH := $(call my-dir)
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
+$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
+	zcat $< > $@
 recovery_uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.cpio
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) ${uncompressed_ramdisk} ${recovery_uncompressed_ramdisk}
 	$(call pretty,"Boot image: $@")
 	$(hide) ./device/samsung/apollo/mkshbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
